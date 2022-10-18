8697082f82873b707ce3e25b5603a430af84c2e3
camuzet jules <cmj4104a@ens.fsi>
2022-10-18 17:16:26 +0200

    fin tp2 et debut tp 3

--- src/personnages/Equipement.java
+++ src/personnages/Equipement.java
@@ -1,5 +1,15 @@
 package personnages;
 
 public enum Equipement {
-
+	CASQUE("casque"), BOUCLIER("bouclier");
+	
+	private String nom;
+	
+	public String toString() {
+		return nom;
+	}
+	
+	private Equipement(String nom) {
+		this.nom = nom;
+	}
 }
--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -2,9 +2,10 @@ package personnages;
 
 
 public class Gaulois  { 
-	private String nom; 
-	private int force; 
+	private String nom;
 	private int effetPotion = 1; 
+	private int force, nbTrophees;
+	private Equipement Trophees[] = new Equipement[100];
 	
 	public Gaulois(String nom, int force) { 
 		this.nom = nom; 
@@ -19,15 +20,30 @@ public class Gaulois  {
 		System.out.println(prendreParole() + "ï¿½ " + texte + "ï¿½"); 
 	} 
 
-	private String prendreParole() { 
-		return "Le gaulois " + nom + " :"; 
-	} 
+//	private String prendreParole() { 
+//		return "Le gaulois " + nom + " :"; 
+//	} 
+	
+	private String prendreParole() {
+		String texte = "Le gaulois " + nom + " : ";
+		return texte;
+	}
 
-	public void frapper(Romain romain) { 
-		System.out.println(nom + " envoie un grand coup dans la machoire de " + romain.getNom()); 
-		romain.recevoirCoup((force / 3) * effetPotion); 
-	} 
+//	public void frapper(Romain romain) { 
+//		System.out.println(nom + " envoie un grand coup dans la machoire de " + romain.getNom()); 
+//		romain.recevoirCoup((force / 3) * effetPotion); 
+//	} 
 
+
+	 public void frapper(Romain romain) { 
+		 System.out.println(nom  +  "  envoie  un  grand  coup  dans  la mâchoire de " + romain.getNom()); 
+		 Equipement  trophees[]  =  romain.recevoirCoup((force  /  3)  * effetPotion); 
+		 for  (int  i  =  0;  trophees  !=  null  &&  i  <  trophees.length;  i++, nbTrophees++) { 
+			 this.trophees[nbTrophees] = trophees[i]; 
+		 } 
+		 
+		 return; 
+	 } 
 
 	public String toString() { 
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]"; 
